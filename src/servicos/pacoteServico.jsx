a4412bea3ed69bc2c044a8c471b154474905d54c
Dev-Russo <murilorusso10@gmail.com>
2021-02-12 19:39:48 -0300

    Pacotes

---
+++ src/servicos/pacoteServico.jsx
@@ -0,0 +1,22 @@
+import http from '../utils/axios';
+
+const listar = () => {
+    return http.get('package', {
+        headers : {
+            'authorization' : `Bearer ${localStorage.getItem('token-codetur')}`
+        }
+    });
+}
+
+const cadastrar = dados => {
+    return http.post('package', JSON.stringify(dados), {
+        headers : {
+            'authorization' : `Bearer ${localStorage.getItem('token-codetur')}`
+        }
+    });
+}
+
+export default {
+    listar,
+    cadastrar
+}
